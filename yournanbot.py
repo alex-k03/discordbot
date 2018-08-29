babfd9c40f56dacf56ec840f6797f1419cb47588
BuildTools <unconfigured@null.spigotmc.org>
2018-08-29 14:18:59 +0100

    added a greeting command
    activated with ///greeting

--- yournanbot.py
+++ yournanbot.py
@@ -5,12 +5,16 @@ import asyncio
 import time
 
 
-bot = commands.Bot(command_prefix='#')
+bot = commands.Bot(command_prefix='///')
 
 #when the bot is ready this will run
 @bot.event
 async def on_ready():
     print("Running on", bot.user.name)
     print("With ID", bot.user.id)
+
+@bot.command(pass_context=True)
+async def greeting(ctx):
+    await bot.say("Hello!")
 
 bot.run("NDg0MzE5Mzk5NzE3ODk2MjEy.DmgSLw.JQMm6f6mFV4zydIKjimi7FgQq6I")
