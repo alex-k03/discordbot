a2a820b1bfb8468c4318fc2c7fe0dcf0c8cd7dab
BuildTools <unconfigured@null.spigotmc.org>
2018-08-29 16:35:43 +0100

    added commands

--- yournanbot.py
+++ yournanbot.py
@@ -1,20 +1,61 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
 import asyncio
-import time
-
+import random
+from datetime import datetime
+from time import strftime
 
 bot = commands.Bot(command_prefix='///')
 
-#when the bot is ready this will run
+
 @bot.event
 async def on_ready():
-    print("Running on", bot.user.name)
-    print("With ID", bot.user.id)
+    print("Running on " + bot.user.name)
+    print("With ID " + bot.user.id)
+
+@bot.command(pass_context=True)
+async def commandhelp(ctx):
+    await bot.say("Available commands: \n greeting \n rolldie \n timedate \n time")
 
 @bot.command(pass_context=True)
 async def greeting(ctx):
     await bot.say("Hello!")
+
+@bot.command(pass_context=True)
+async def rolldie(ctx):
+    await bot.say(random.randint(1,6))
+
+@bot.command(pass_context=True)
+async def timedate(ctx):
+    await bot.say(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+@bot.command(pass_context =True)
+async def doashit(ctx):
+    await bot.say(":poop:" * 100)
+
+@bot.command(pass_context=True)
+async def time(ctx):
+    hour = int(datetime.now().strftime('%H'))
+    minutes = int(datetime.now().strftime('%M'))
+
+    if hour > 12:
+        hour = hour - 12
+
+    if minutes > 10 and minutes < 15:
+        a = ' ten past '
+    elif minutes > 15 and minutes < 30:
+        a = ' quarter past '
+    elif minutes > 30 and minutes < 45:
+        a = ' half past '
+    elif minutes > 45 and minutes < 50:
+        a = ' quarter to '
+        hour = hour + 1
+    elif minutes > 50 and minutes < 59:
+        a = ' ten to '
+        hour = hour + 1
+    else:
+        a = ''
+
+    await bot.say("It is" + str(a) + str(hour))
 
 bot.run("NDg0MzE5Mzk5NzE3ODk2MjEy.DmgSLw.JQMm6f6mFV4zydIKjimi7FgQq6I")
