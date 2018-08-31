5478163e37ab1f7c7216ba54ab173e13c4e70f5d
BuildTools <unconfigured@null.spigotmc.org>
2018-08-31 18:05:26 +0100

    fixed voice command in comms.py (-join) (big up rudi for helping)

--- commands/interact/comms.py
+++ commands/interact/comms.py
@@ -1,24 +1,21 @@
 from discord.ext import commands
-
 #this is for the bot to make communications with the user
 class comms():
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context = True)
+    @commands.command(pass_context=True)
     async def join(self, ctx):
-        channel = ctx.message.author.voice.voice_channel
-        await bot.join_voice_channel(channel)
+        if ctx.message.author.voice:
+            channel = ctx.message.author.voice.channel
+            await channel.connect(timeout=60.0, reconnect=True)
 
-    @commands.command(pass_context = True)
+    @commands.command(pass_contex=True)
     async def leave(self, ctx):
         for x in bot.voice_clients:
             if(x.server == ctx.message.server):
                 return await x.disconnect()
-
-
-
 
 def setup(bot):
     bot.add_cog(comms(bot))
