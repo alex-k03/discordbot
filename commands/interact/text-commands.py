332750852f19a7cd9c91f5c39179e55adb610c3d
BuildTools <unconfigured@null.spigotmc.org>
2018-08-31 12:21:22 +0100

    missed from last commit

--- commands/interact/text-commands.py
+++ commands/interact/text-commands.py
@@ -5,15 +5,6 @@ class text_commands():
     def __init__(self, bot):
         self.bot = bot
 
-<<<<<<< HEAD
-    @bot.command(pass_context=True)
-    async def greeting(ctx):
-        await ctx.send("Hello! :shake:")
-
-    @bot.command(pass_context=True)
-    async def rolldie(ctx):
-        await ctx.send(random.randint(1,6))
-=======
     @commands.command(pass_context=True)
     async def greeting(self, ctx):
         await ctx.send("Hello! :shake:")
@@ -29,7 +20,6 @@ class text_commands():
     @commands.command(pass_context = True)
     async def doggo(self, ctx):
         await ctx.send("https://giphy.com/gifs/dog-shiba-inu-typing-mCRJDo24UvJMA")
->>>>>>> b9698f88f0cbe673dae8531f58efbb4ffeb6215a
 
 def setup(bot):
     bot.add_cog(text_commands(bot))
